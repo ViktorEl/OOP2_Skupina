a9b9141fa10d782a7cdc3f185634eac1e4b4d065
Your Name <you@example.com>
2025-01-10 10:45:12 +0100

    Vytvorenie a doplnenie triedy Robot

---
+++ robot.py
@@ -0,0 +1,16 @@
+
+class Robot():
+
+    def __init__(self, meno, hmotnost): # inicializacna metoda/konštruktor
+        self.meno = meno                # inštančná premenná
+        self.hmotnost = hmotnost
+
+    def predstav_sa(self):
+        print(f'Volám sa {self.meno} a moja hmotnost je {self.hmotnost}')
+
+
+objekt1 = Robot('Jozo', 120) # vytvorenie objektu/inštancia 
+print(objekt1.meno)          # pristupujem k atributu meno daneho objetku
+objekt1.predstav_sa()        # pristupujem k metode, ktorý ma daný objekt k dispozicii
+
+objekt2 = Robot('Mat', 150) # vytvorenie druhého objektu
