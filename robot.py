31c22e0f7e8278478edbae155fd9b808be5127c9
Your Name <you@example.com>
2025-01-16 09:24:50 +0100

    Vytvorenie prveho setteru

--- robot.py
+++ robot.py
@@ -8,6 +8,11 @@ class Robot():
     def predstav_sa(self):
         print(f'Volám sa {self.meno} a moja hmotnost je {self.hmotnost}')
 
+    def set_meno(self, meno):
+        if len(meno) < 4:
+            raise ValueError('chyba kratke meno')
+
+
 
 objekt1 = Robot('Jozo', 120) # vytvorenie objektu/inštancia 
 print(objekt1.meno)          # pristupujem k atributu meno daneho objetku
