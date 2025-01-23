6caba43d37acf8dfd9a8cf4217a8a69434b2b893
Your Name <you@example.com>
2025-01-23 09:24:51 +0100

    vytvorenie setteru pre značku

--- auto.py
+++ auto.py
@@ -6,7 +6,17 @@ class Auto():
         self.model = model
         self.rok_vyroby = rok_vyroby
 
+    def vypis_parametre(self):
+        print(f'Značka auta:{self.znacka}, model: {self.model}, rok: {self.rok_vyroby}')
+
+    def set_znacka(self, znacka):
+        if znacka == "":
+            raise ValueError('chyba nezadali ste nič')
+        if not isinstance(znacka,str):
+            pass
+
 
 
 
 auto1 = Auto('Skoda', 'Fabia', 2020)
+auto1.vypis_parametre()
