4349f12dda2cb7699aa9bcc476768903f1d988b8
Your Name <you@example.com>
2025-02-13 09:05:27 +0100

    doplnenie metód do triedy MestoObyvatelia

--- mesto.py
+++ mesto.py
@@ -13,9 +13,18 @@ class MestoObyvatelia():
     def pocet_obyvatelov_podla_veku(self, vek):
         pocitadlo = 0
         for obyvatel in self.obyvatelia:
-            if vek == obyvatel.vek:
+            if vek == obyvatel.get_vek():
                 pocitadlo += 1
         return pocitadlo
+
+    def pocet_podla_pohlavia(self, pohlavie):
+        pocitadlo = 0
+        for obyvatel in self.obyvatelia:
+            if pohlavie == obyvatel.get_pohlavie():
+                pocitadlo += 1
+        return pocitadlo
+
+
 
 
 roznava = MestoObyvatelia('roznava')
@@ -23,6 +32,7 @@ roznava.uloz_obyvatela('Jozo', 40, 'm')
 roznava.uloz_obyvatela('Lucia', 27, 'z')
 roznava.uloz_obyvatela('Milan', 27, 'm')
 print(roznava.pocet_obyvatelov_podla_veku(27))
+print(roznava.pocet_podla_pohlavia('m'))
 
 
 
--- obyvatel.py
+++ obyvatel.py
@@ -4,10 +4,14 @@ class Obyvatel():
 
     def __init__(self, meno, vek, pohlavie):
         self.meno = meno
-        self.vek = vek
-        self.pohlavie = pohlavie
+        self.__vek = vek
+        self.__pohlavie = pohlavie
     
+    def get_vek(self):
+        return self.__vek
 
+    def get_pohlavie(self):
+        return self.__pohlavie
 
 
 #obyvatel1 = Obyvatel('Jozo', 18, 'm')
