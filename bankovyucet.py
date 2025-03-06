72aff93b4da3ce262ef760e1931156a5878994e7
Your Name <you@example.com>
2025-03-06 09:08:58 +0100

    uloha bankovy ucet

---
+++ bankovyucet.py
@@ -0,0 +1,50 @@
+
+class BankovyUcet():
+
+    def __init__(self ,majitel, zostatok=0):
+        self.set_majitel(majitel)
+        self.zostatok = zostatok
+
+
+    def set_majitel(self , majitel):
+        if majitel == "":
+            raise ValueError('majitel nemoze byt prazdny')
+        if not isinstance(majitel , str):
+            raise ValueError('majitel neni string')
+        self.__majitel = majitel
+
+
+    def set_zostatok(self , zostatok):
+        if not isinstance(zostatok , (int, float)):
+            raise ValueError('zostatok neni int ani float')
+        self.zostatok = zostatok
+    
+
+    def get_majitel(self):
+        return self.__majitel
+
+    def get_zostatok(self):
+        return self.zostatok
+
+    def vklad(self , vklad):
+        self.zostatok = self.zostatok + vklad
+
+
+    def vyber(self , vyber):
+        if vyber > self.zostatok:
+            raise ValueError('prečo bereš viac ako maš')
+        else:
+            self.zostatok = self.zostatok - vyber
+
+
+    def __str__(self):
+        return f'{self.__majitel} {self.zostatok}'
+
+
+
+ucet1 = BankovyUcet('Jozo')
+print(ucet1)
+
+
+
+        
