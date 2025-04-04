a0ebf50f18145a2a2d1454b7b94b6bbd1f919fd0
Your Name <you@example.com>
2025-04-04 10:00:31 +0200

    trieda robot a gui_robot

--- rpg_hra.py
+++ rpg_hra.py
@@ -1,3 +1,5 @@
+import random
+
 class Postava():
 
     def __init__(self, meno, uroven, zivoty):
@@ -37,3 +39,48 @@ class Postava():
 
     def utok(self):
         return 'Postava útočí'
+    
+
+class Bojovnik(Postava):
+
+    def __init__(self, meno, uroven, zivoty, sila):
+        super().__init__(meno, uroven, zivoty)
+        self.set_sila(sila)
+
+    def set_sila(self, sila):
+        if not isinstance(sila, int):
+            raise ValueError('Sila musi byt cislo')
+        if sila < 1 or sila > 10:
+            raise ValueError('sila musi byt v rozsahu od 1 do 10')
+        self.sila = sila
+
+    def utok(self):
+        nahodny_utok = random.randint(1, self.sila)
+        return f'Bojovnik zautocil mečom silou {nahodny_utok}'
+    
+class Kuzelnik(Bojovnik):
+
+    def __init__(self, meno, uroven, zivoty, sila, mana):
+        super().__init__(meno, uroven, zivoty, sila)
+        self.set_mana(mana)
+
+    def set_mana(self, mana):
+        if mana < 0 or mana > 50:
+            raise ValueError('Mana musi byt v rozsahu od 0 do 50')
+        self.mana = mana
+
+    def set_sila(self, sila):
+        if not isinstance(sila, int):
+            raise ValueError('Sila musi byt cislo')
+        if sila < 1 or sila > 20:
+            raise ValueError('sila musi byt v rozsahu od 1 do 20')
+        self.sila = sila 
+
+    def utok(self):
+        nahodna_sila = random.randint(1, self.sila)
+        pass
+    
+
+
+
+
